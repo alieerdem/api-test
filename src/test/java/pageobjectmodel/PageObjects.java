7eeb590ae3b9990c097f09f573af8bc63463f363
Ali Ertugrul Erdem <alie2@outlook.com>
2019-09-25 13:15:07 +0300

    first commit

---
+++ src/test/java/pageobjectmodel/PageObjects.java
@@ -0,0 +1,10 @@
+package pageobjectmodel;
+
+public class PageObjects {
+		//My api key. It is requested for all requests at http://www.omdbapi.com
+		public static final String API_KEY = "d0cc8655";
+		//Movie title to search for
+		public static String MOVIE_TITLE_TO_SEARCH="Harry Potter";
+		//imdbID of movie that's title equals to MOVIE_TITLE_FOR_ID will be parsed
+		public static String MOVIE_TITLE_FOR_ID="Harry Potter and the Sorcerer\\'s Stone";
+}
---
+++ src/test/java/tests/ApiTest.java
@@ -0,0 +1,73 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pageobjectmodel.PageObjects;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ApiTest {
+	
+	public ApiTest() {
+		baseURI="http://www.omdbapi.com";
+	}
+	
+	@Test
+	public void checkStatusCode() {
+		given().
+			param("apikey", PageObjects.API_KEY).
+			param("i", getID()).
+		when().
+			get().
+		then().
+			statusCode(200);
+	}
+	
+	@Test
+	public void checkTitle() {
+		given().
+			param("apikey", PageObjects.API_KEY).
+			param("i", getID()).
+		when().
+			get().
+		then().
+			body("$", hasKey("Title")).
+			body("Title.size()", greaterThan(0));
+	}
+	
+	@Test
+	public void checkYear() {
+		given().
+			param("apikey", PageObjects.API_KEY).
+			param("i", getID()).
+		when().
+			get().
+		then().
+			body("$", hasKey("Year")).
+			body("Year.size()", greaterThan(0));
+	}
+	
+	@Test
+	public void checkReleased() {
+		given().
+			param("apikey", PageObjects.API_KEY).
+			param("i", getID()).
+		when().
+			get().
+		then().
+			body("$", hasKey("Released")).
+			body("Released.size()", greaterThan(0));
+	}
+	
+	//function to get imdbID of the movie which is title "Harry Potter and the Sorcerer's Stone" (can be changed on PageObjects.movieTitleForID) 
+	private String getID () {
+		String imdbID =
+				given().
+					param("apikey", PageObjects.API_KEY).
+					param("s", PageObjects.MOVIE_TITLE_TO_SEARCH).
+				when().
+					get().
+				then().
+					extract().response().path("Search.find{it.Title=='"+PageObjects.MOVIE_TITLE_FOR_ID+"'}.imdbID");
+		return imdbID;
+	}
+}
